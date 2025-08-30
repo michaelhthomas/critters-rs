18056bd5671ea0548fbb9741aa8dd080c6591c63
github-action-benchmark <github@users.noreply.github.com>
2025-08-30 14:23:10 +0000

    add critters_rs (cargo) benchmark result for 1b19f2c05773f429863221b5e12161f5a93faa16

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756563790292,
+  "repoUrl": "https://github.com/michaelhthomas/critters-rs",
+  "entries": {
+    "critters_rs": [
+      {
+        "commit": {
+          "author": {
+            "email": "michaelhthomas@outlook.com",
+            "name": "Michael Thomas",
+            "username": "michaelhthomas"
+          },
+          "committer": {
+            "email": "michaelhthomas@outlook.com",
+            "name": "Michael Thomas",
+            "username": "michaelhthomas"
+          },
+          "distinct": true,
+          "id": "1b19f2c05773f429863221b5e12161f5a93faa16",
+          "message": "ci: add benchmarks",
+          "timestamp": "2025-08-30T10:19:48-04:00",
+          "tree_id": "0c4a3589630f144e23657f9f0dbf560e070dfc2c",
+          "url": "https://github.com/michaelhthomas/critters-rs/commit/1b19f2c05773f429863221b5e12161f5a93faa16"
+        },
+        "date": 1756563789466,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "inline_rust_wikipedia",
+            "value": 345694068,
+            "range": "± 7798971",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
