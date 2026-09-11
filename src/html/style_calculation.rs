62e7f1ff16501f391eba503b57b0e57200c58e0e
Michael Thomas <michaelhthomas@outlook.com>
2026-09-10 20:39:54 -0400

    perf(style_calculation): hash rule pointers rather than selectors
    Avoids the slight performance degredation resulting from the switch from
    interned strings to the `lightningcss` `CowArcStr` for selectors

--- src/html/style_calculation.rs
+++ src/html/style_calculation.rs
@@ -13,7 +13,7 @@ use smallvec::SmallVec;
 use std::collections::{HashMap, HashSet};
 
 /// A CSS rule with its selector and the data needed to match it quickly.
-#[derive(Debug, Clone, Eq)]
+#[derive(Debug, Clone)]
 struct Rule<'i> {
     /// The CSS selector for this rule
     pub selector: Selector<'i>,
@@ -29,15 +29,24 @@ impl<'i> Rule<'i> {
     }
 }
 
-impl std::hash::Hash for Rule<'_> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.selector.hash(state);
+/// A reference to a Rule which is hashed by address.
+///
+/// Since rules are stored in a RuleSet throughout the matching process,
+/// their pointers are stable and faster to hash than the whole selector.
+#[derive(Debug, Clone, Copy)]
+struct RuleRef<'a, 'i>(&'a Rule<'i>);
+
+impl Eq for RuleRef<'_, '_> {}
+
+impl PartialEq for RuleRef<'_, '_> {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self.0, other.0)
     }
 }
 
-impl PartialEq for Rule<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.selector == other.selector
+impl std::hash::Hash for RuleRef<'_, '_> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_usize(self.0 as *const Rule<'_> as usize);
     }
 }
 
@@ -179,7 +188,7 @@ fn calculate_matching_rules<'a, 'i>(
     element: &NodeDataRef<ElementData>,
     rule_set: &'a RuleSet<'i>,
     bloom: &mut StyleBloom,
-    rules: &mut HashSet<&'a Rule<'i>>,
+    rules: &mut HashSet<RuleRef<'a, 'i>>,
 ) {
     if cfg!(debug_assertions) {
         bloom.assert_complete(element.clone());
@@ -203,7 +212,7 @@ fn calculate_matching_rules<'a, 'i>(
             .selector
             .matches_with_context(element, Some(&rule.hashes), &mut context)
         {
-            rules.insert(rule);
+            rules.insert(RuleRef(rule));
         }
     }
 }
@@ -247,7 +256,7 @@ pub fn calculate_styles_for_tree<'i>(
 
     rules
         .into_iter()
-        .map(|rule| rule.selector.clone())
+        .map(|rule| rule.0.selector.clone())
         .collect()
 }
 
